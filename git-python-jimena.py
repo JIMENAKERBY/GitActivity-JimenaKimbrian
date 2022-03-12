1c0e660a4007be1e8ef119d230ccbe9a3242f3e0
JimenaKimbrian <Jimenakerby@gmail.com>
2022-03-12 11:28:45 -0800

    My Educational Background added

--- git-python-jimena.py
+++ git-python-jimena.py
@@ -7,3 +7,6 @@ Created on Sat Mar 12 10:46:31 2022
 print("Kim Brian L. Jimena")
 print("Mataas na Bayan Boac, Marinduque")
 print("February 6, 2002")
+print("Marinduque State College")
+print("BS Information Technology")
+print("2nd Year, Section D")
