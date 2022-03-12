96db024cc9345f99c2711254c00a7017b86e6c14
JimenaKimbrian <Jimenakerby@gmail.com>
2022-03-12 11:07:28 -0800

    My Personal Info added

---
+++ git-python-jimena.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 10:46:31 2022
+
+@author: Kerby
+"""
+print("Kim Brian L. Jimena")
+print("Mataas na Bayan Boac, Marinduque")
+print("February 6, 2002")
